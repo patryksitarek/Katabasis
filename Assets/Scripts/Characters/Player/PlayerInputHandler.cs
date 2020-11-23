defacb9fe34dab4ee3e149e3efeefd008b67cb35
mhubl <hubala.mateusz@gmail.com>
2020-11-24 00:29:15 +0100

    Prepared file structure for scripts, added PlayerInputHandler.
    For some reason a lot of other files were marked as modified, but everything is still working so w/e

---
+++ Assets/Scripts/Characters/Player/PlayerInputHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+
+    public float movementSpeed = 7;
+    public Animator animator;
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        agent.updateRotation = false;    
+    }
+    
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        MoveBy(h, v);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            // attack
+        }
+    }
+
+    void MoveBy(float horizontal, float vertical)
+    {
+        if (horizontal != 0 || vertical != 0)
+        {
+            // animator.SetBool("isRunning", true);
+
+            // if (!AudioManager.Instance.isSFXPlaying()) AudioManager.Instance.PlaySFX(walkSound);
+        }
+        else
+        {
+            // animator.SetBool("isRunning", false);
+        }
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * movementSpeed * Time.deltaTime;
+        agent.Move(-movement);
+    }
+}
