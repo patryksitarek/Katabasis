b061a6fa2220b77daa314f65ca30bd8a54483e37
mhubl <hubala.mateusz@gmail.com>
2020-11-26 02:46:02 +0100

    Added player following functionality to the camera

---
+++ Assets/Scripts/Objects/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public GameObject map;
+    public float offset = 9f;
+
+    private float mapSize;
+
+    private void Start()
+    {
+        mapSize = map.GetComponent<MeshCollider>().bounds.size.x / 2;
+        transform.position = new Vector3(
+            player.transform.position.x,
+            transform.position.y,
+            transform.position.z
+        );
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        
+        // Ensure the camera doesn't leave the map
+        if (Math.Abs(newPosition.x) < mapSize - offset)
+        {
+            transform.position = newPosition;
+        }
+    }
+}
