2d90c9b92e233705f5d2b2a5849b2280057dd327
patryksitarek <58287067+patryksitarek@users.noreply.github.com>
2021-01-20 23:57:44 +0100

    Dialogs

---
+++ Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour
+{
+
+    private DialogueManager dMan;
+
+    private bool hasEntered;
+
+    public string[] dialogueLines;
+    public string dialogAuthor;
+
+    void Start()
+    {
+        dMan = FindObjectOfType<DialogueManager>();
+    }
+    
+  
+    void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.name == "Player Knight" && !hasEntered)
+        {
+            hasEntered = true;
+            Debug.Log("Player has entered");
+            //dMan.ShowBox(dialogue);
+
+            if (!dMan.dialogActive)
+            {
+                dMan.dialogLines = dialogueLines;
+                dMan.dialogAuthor = dialogAuthor;
+                dMan.currentLine = 0;
+                dMan.ShowDialogue();
+            }
+
+            
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public GameObject dBox;
+    public Text dText;
+    public Text dAuthor;
+
+    public bool dialogActive;
+
+    public string[] dialogLines;
+    public string dialogAuthor;
+    public int currentLine;
+
+
+
+    void Update()
+    {
+        if (dialogActive && Input.GetKeyDown(KeyCode.E)) 
+        {
+            currentLine++;
+        }
+
+        if (currentLine >= dialogLines.Length)
+        {
+            dBox.SetActive(false);
+            dialogActive = false;
+            currentLine = 0;
+            Time.timeScale = 1;
+        }
+
+        dText.text = dialogLines[currentLine];
+        dAuthor.text = dialogAuthor;
+    }
+
+    public void ShowBox(string dialogue)
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+        dText.text = dialogue;
+    }
+
+    public void ShowDialogue()
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+    }
+}
